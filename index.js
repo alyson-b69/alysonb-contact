5d4b4371f82687bd98b90dc83ed95c2c458d3eda
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-22 10:39:07 +0200

    add object on header request

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://alyson-b.netlify.app");
+  res.header("Access-Control-Allow-Origin :", "https://alyson-b.netlify.app");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers :",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
