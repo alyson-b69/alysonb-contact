c48010f8afa96574bc3a45f091127aff6e9f91a6
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-22 10:07:36 +0200

    spelling mistake

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://alyson-b.netlify.app");
   res.header(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
