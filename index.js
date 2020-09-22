65cead3c427c7e227ede656ef95bac05bc923e29
alyson-b69 <alyson.bernabeu@gmail.com>
2020-09-22 11:29:32 +0200

    add secure change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://alyson-b.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -61,8 +61,3 @@ app.listen(process.env.PORT || 5000, () => {
   const port = process.env.PORT;
   console.log("Server started at", port);
 });
-
-// const server = app.listen(4000, function () {
-//   const port = server.address().port;
-//   console.log("Server started at http://localhost:%s", port);
-// });
