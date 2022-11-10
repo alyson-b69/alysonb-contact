295d6bc8e60533f07b53451fda5ddf2f72368ed8
alyson-b69 <alyson.bernabeu@gmail.com>
2022-11-10 14:40:40 +0100

    change gmail for sfr mail to fix security error on google + add template html to improve mail format

--- index.js
+++ index.js
@@ -1,68 +1,76 @@
 const express = require("express");
 const nodeMailer = require("nodemailer");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv");x
+
+const cors = require('cors')
 dotenv.config();
 
 const app = express();
+app.use(cors())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://alyson-b.netlify.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
 });
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 3 * 60 * 1000,
-  max: 30, // per IP
+    windowMs: 3 * 60 * 1000,
+    max: 30, // per IP
 });
 
 app.use(limiter);
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post("/send-email", function (req, res, next) {
-  const transporter = nodeMailer.createTransport({
-    host: "smtp.gmail.com",
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-    port: 465,
-    secure: true,
-  });
+app.post("/send-email", (req, res, next) => {
 
-  const mailOptions = {
-    from: req.body.email,
-    to: process.env.EMAIL,
-    subject: req.body.subject,
-    html:
-      req.body.name +
-      " (" +
-      req.body.email +
-      ") " +
-      " send this message : " +
-      req.body.message,
-  };
+    const transporter = nodeMailer.createTransport({
+        host: "smtp.sfr.fr",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
+    });
 
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.status(200).json({
-        success: true,
-        message: "Email Sent",
-      });
-    }
-  });
+    const mailOptions = {
+        from: `Alyson CV <${process.env.EMAIL}>`,
+        to: process.env.EMAILTOSEND,
+        subject: req.body.subject,
+        html: `<html lang="fr">
+                <body>
+                    <div>
+                         <header style="background: #fa6862; color: #ffffff; padding: 8px 12px">${req.body.name} ( ${req.body.from} ) a envoyé ce message : </header>
+                         <div style="color: #111827; padding: 8px 12px" >${req.body.message.replaceAll('\n', '<br />')}</div>
+                    </div>
+                </body>
+              </html>`,
+    };
+
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+             res.send(err);
+        } else {
+             res.status(200).json({
+                success: true,
+                message: "Email_sent",
+                info: info
+            });
+        }
+    })
+
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  const port = process.env.PORT;
-  console.log("Server started at", port);
+app.listen(process.env.PORT || 5001, () => {
+    const port = process.env.PORT;
+    console.log("Server started at", port);
 });
